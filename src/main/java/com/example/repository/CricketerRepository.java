7cc663151454b81fc5ef6f8e9ccaaaa6357f88f6
Nirmal Kumar Sekar <kagosekar@gmail.com>
2017-12-19 08:04:59 +0530

    Fixed missing annotation

--- src/main/java/com/example/repository/CricketerRepository.java
+++ src/main/java/com/example/repository/CricketerRepository.java
@@ -1,9 +1,11 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.model.Cricketer;
 
+@Repository
 public interface CricketerRepository extends JpaRepository<Cricketer,Long>{
 
 }
